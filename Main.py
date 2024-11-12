4e8db906e512e1f16a5c203c90d05f86a3394122
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2024-11-12 17:39:32 +0700

    hari kedua

--- Main.py
+++ Main.py
@@ -1,1 +1,13 @@
 print("hello world");
+# -Irfan ini adalah symbol untuk comment di pyton sama halnya di dart jika sebuah baris ada komen makan sebuah kode progama maka akan diabaika alias tidak di anngab sebuah kode
+
+""" 
+didalam bahasa pemograman pyto juga ada namanya multiline comment sama halnya dengan  di flutter mulltiline fungsinya sebagai  komment di banyak baris
+"""
+
+# -Irfan jadi di pyton sebuah kode jika di jalankan akan sesuai barisan dari atas samapai bahaw
+# -Irfan jika kita membuat variabel tapi di dahului kata kunci print value yang kita buat mamka akan error
+# contoh
+
+a = 2 #maka akan eror
+print(a)
