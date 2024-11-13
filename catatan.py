cbe5eac4d13d16b4ec5727c67f56f54fbcc4e2f2
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2024-11-13 13:43:52 +0700

    .

---
+++ catatan.py
@@ -0,0 +1,2 @@
+#-Irfan jika kita ingin membuka python sheel di vs code  maka kita hanya perlua mengetika python di terminal
+# dan jika kita ingin menutup python sheel kita hanya perlu mengetikan exit()
---
+++ variabel.py
@@ -0,0 +1,17 @@
+# varianel adalah tempat menyimpan sebuah nilai atau value
+a = 10;
+
+print("nilai a =", a);  #ini adalaha cara di mana bahasa pemoograman python menempilkan teks dan memangil nilai a berbeda di flutter 
+
+
+# didalam bahasa pemograman python ada aturan khusus dalam penamaan variabel
+# 1.nama variabele tidak boleh di dahulu underscore
+# 2.nama varibale tidak boleh di dahului angka
+# case sensitive di mana huruf anka besar dan kecil adalah dua varianel yang berbeda
+# seperti ular dan Ular ini  adalah dua variabel yang berbeda
+
+
+# asigmen indirict
+
+b = a
+print(b)
