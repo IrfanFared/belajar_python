68f28d54e1871deac4650475ba80720199581e23
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2024-11-29 17:40:30 +0700

    .

---
+++ operator_asigment.py
@@ -0,0 +1,63 @@
+#- Irfan operator asiggment (operatator pengugasan yang lebih singkat) di python di gunakan untuk memberikan nilai ke sebuah variabel.
+#operator ini juga  memunkinkn kombinasi dengan oprasi aritmatika ,logika dan lainnya untuk mempersingkat kode
+
+#intinya oprator asigmen di gunakan untu mempersingkat kode
+
+#penjumlahan assigment
+x = 100
+x += 5  # Sama dengan x = x + 5
+print(x)  # Output: 15
+
+#pengurangan assiment
+x -= 2
+print(x)
+
+#perkalian assigment
+a = 10
+a *= 10
+print(a)
+
+
+#perkalian asigment
+a /= 2
+print(a)
+
+#modulus assigment
+b = 12
+b %= 5
+print(b)
+
+#pembagian integer asigment
+x = 10
+x //= 3  # Sama dengan x = x // 3
+print(x)  # Output: 3
+
+#eksponesial asigment
+x = 2
+x **= 3  # Sama dengan x = x ** 3
+print(x)  # Output: 8
+
+#Bitwise AND dan Assignment:
+x = 6  # biner: 110
+x &= 3  # Sama dengan x = x & 3 (biner: 011)
+print(x)  # Output: 2 (biner: 010)
+
+#Bitwise OR dan Assignment
+x = 6  # biner: 110
+x |= 3  # Sama dengan x = x | 3 (biner: 011)
+print(x)  # Output: 7 (biner: 111)
+
+#Bitwise XOR dan Assignment
+x = 6  # biner: 110
+x ^= 3  # Sama dengan x = x ^ 3 (biner: 011)
+print(x)  # Output: 5 (biner: 101)
+
+#Shift Right dan Assignment
+x = 8  # biner: 1000
+x >>= 2  # Sama dengan x = x >> 2
+print(x)  # Output: 2 (biner: 10)
+
+#Shift Left dan Assignment
+x = 3  # biner: 11
+x <<= 2  # Sama dengan x = x << 2
+print(x)  # Output: 12 (biner: 1100)
