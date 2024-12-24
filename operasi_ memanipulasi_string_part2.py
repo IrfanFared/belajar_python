c5e945a21fef4df48b5553398d958efa8fc6c1a0
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2024-12-24 10:53:41 +0700

    besok belajar episode 18

--- operasi_ memanipulasi_string_part2.py
+++ operasi_ memanipulasi_string_part2.py
@@ -40,3 +40,61 @@ nama_lengkap = "Irfan Pratama".startswith("Irfan")
 print(nama_lengkap)
 nama_lengkap_p = "Irfan Pratamax".endswith("Pratamax")
 print(nama_lengkap_p)
+
+# Metode  split() digunakan untuk memisahkan string ke bebeberapa substring  berdasarkan karakter pemisah  fungsi ini mengebalikan nilai dalam bentuk list
+
+# Memisahkan berdasarkan spasi
+string = "Python adalah bahasa pemrograman"
+result = string.split()
+print(result)  # Output: ['Python', 'adalah', 'bahasa', 'pemrograman']
+
+# Memisahkan berdasarkan koma
+data = "merah, hijau, biru"
+result = data.split(', ')
+print(result)  # Output: ['merah', 'hijau', 'biru']
+
+
+# sebaliknya  metode join() digunakan untuk elemen dari list menjadi strin dengan mengunakan karakter pemisah yang sudah ditentukan
+# Menggabungkan list menjadi string
+words = ["Hello", "World", "from", "Python"]
+result = " ".join(words)
+print(result)  # Output: Hello World from Python
+
+# Menggabungkan dengan pemisah tertentu
+data_siswa = ['Gracia Violin', 'kelas 4', 'SKC Jakarta']
+siswa_join = '|'.join(data_siswa)
+print(siswa_join)  # Output: Gracia Violin|kelas 4|SKC Jakarta
+
+
+# metode rjust digunakan untuk mengatur teks ke kanan dalam string dengan menambahakan karakter pengisis di sebelah kiri
+
+text = 'Python'
+print(text.rjust(10))        # Output: '    Python'
+print(text.rjust(10, '*'))   # Output: '****Python'
+
+# metode ljust digunakan untuk mengatur teks ke kiri dalam string dengan menambahakan   karakter pengisis di sebelah kanan
+
+text = 'Python'
+print(text.ljust(10))        # Output: 'Python    '
+print(text.ljust(10, '*'))   # Output: 'Python****'
+
+# metode center  digunakan untuk memusatkan teks ke dalam string dengan menambakahkan karakter antar  kedua sisi
+
+text = 'Python'
+print(text.center(10))       # Output: '  Python  '
+print(text.center(10, '*'))  # Output: '**Python**'
+
+#Metode strip() dalam Python adalah fungsi bawaan yang digunakan untuk menghapus karakter tertentu dari awal dan akhir string. Secara default, metode ini menghapus spasi putih, tetapi juga dapat digunakan untuk menghapus karakter lain jika ditentukan.
+
+#Menghapus Spasi Putih:
+
+text = "   Hello, World!   "
+cleaned_text = text.strip()
+print(cleaned_text)  # Output: 'Hello, World!'
+
+#Menghapus Karakter Tertentu:
+
+text = "****Hello, World!****"
+cleaned_text = text.strip("*")
+print(cleaned_text)  # Output: 'Hello, World!'
+
