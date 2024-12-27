b6ba9836fa618faa9fea25658c09fabb4bb677c8
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2024-12-27 10:21:48 +0700

    .

--- format_string.py
+++ format_string.py
@@ -19,7 +19,7 @@
 
 Nama = "Irfan"
 No_absen = 21
-Biodata = "Ini adalah biodataku Nama %s No absen %d" % (Nama, No_absen) # ya ini mirip kaya string iterpolation di flutter
+Biodata = "Ini adalah biodataku Nama %s No absen %d" % (Nama, No_absen) # ya ini mirip kaya string iterpolation di flutter ouput  kode ini adalah Ini adalah biodataku Nama Irfan No absen 21
 print(Biodata)
 
 #metode format() di stirng adalah metode yang digunakan untuk melakukan format string dengan menyisipkan variabel atau nilai ke dalam string.
@@ -28,7 +28,7 @@ print(Biodata)
 #contoh 
 Nama = "Irfan" 
 No_absen = 21
-Biodata = "Ini adalah biodataku Nama {} No absen {}".format(Nama, No_absen) 
+Biodata = "Ini adalah biodataku Nama {} No absen {}".format(Nama, No_absen)  #ouput dari kode ini adalah = Ini adalah biodataku Nama Irfan No absen 21
 print(Biodata)
 
 #f string di bahasa pemogrman python adalah metode yang digunakan untuk melakukan format string dengan menyisipkan variabel atau nilai ke dalam string.
@@ -39,5 +39,44 @@ print(Biodata)
 # contoh
 Nama = "Irfan"
 No_absen = 21
-Biodata = f"Ini adalah biodataku Nama {Nama} No absen {No_absen}"
+Biodata = f"Ini adalah biodataku Nama {Nama} No absen {No_absen}" #output dari kode ini adalah = Ini adalah biodataku Nama Irfan No absen 21
 print(Biodata)
+
+# kita dapat menamabahka :d setelah variabel untuk menentukan tipe data yang akan di tampilkan memebuat bilangan tersebt menjadi bilangan bulat
+#contoh
+umur = 21
+Biodata = f"Ini adalah biodataku Nama {Nama} No absen {No_absen} Umur {umur:d}" #output = 21
+print(Biodata)
+
+# kita dapat menambahkan :.2f setelah variabel untuk menentukan tipe data yang akan di tampilkan memebuat bilangan tersebt menjadi bilangan pecahan
+# siyntax :{variabel:.2f}
+#contoh
+nilai = 90.521
+Biodata = f"Ini adalah biodataku Nama {Nama} No absen {No_absen} Nilai {nilai:.2f}" #output = 90.52
+print(Biodata)
+
+# kita bisa menambakahkan :, setelah {variabel} untuk menambahkan pemisah ribuan    
+#contoh
+uang = 10000000000000
+Biodata = f"Ini adalah biodataku Nama {Nama} No absen {No_absen} Uang {uang:,}"# output = 10,000,000,000,000
+print(Biodata)
+
+#kita bisa menambahkan  ledding zero set di dalam bahasa pemograman python untuk menambahkan angka 0 di depan angka
+# untuk symtaxnya {variabel:02d} jika bentuknya pecahan atau float {variabel:02f}
+#contoh untuk bilangan bulat
+umur = 21
+Biodata = f"Ini adalah biodataku Nama {Nama} No absen {No_absen} Umur {umur:010d}" #output = 21
+print(Biodata)
+
+#contoh untuk bilangan pecahan
+nilai = 90.521
+Biodata = f"Ini adalah biodataku Nama {Nama} No absen {No_absen} Nilai {nilai:010.2f}" #output = 90.52
+
+# di dalama bahasa python kit bisa menapilkan tanda minus dapan plus di depan angka cara menambahkannya adalah dengan menambahkan simbol + atau - setelah : 
+# untuk sytaxnya {variabel:+d} jika bentuknya pecahan atau float {variabel:+f}
+#contoh 
+
+uang = -1023.23552
+Biodata = f"Ini adalah biodataku Nama {Nama} No absen {No_absen} Uang {uang:+.2f}" #output = -1023.24
+print(Biodata)
+
