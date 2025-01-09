4bf0fbe5905b3b8ea5dbfc7272d4a052a43c85b4
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2025-01-09 21:20:53 +0700

    Add example of while loop in Python with explanation and implementation

---
+++ while_loop.py
@@ -0,0 +1,13 @@
+# while loop  didalam bahsa pemograman python adalah perulangan yang digunakan untuk mengulang kode tertentu selama kondisi tertentu bernilai benar.
+# cara kerjanya adalah
+# while kodisi:
+#     statement(s)
+
+
+angka = 0
+
+while angka < 4:
+    angka +=1
+    print(f"perulangan ke {angka}")
+
+print("progam selesai")
