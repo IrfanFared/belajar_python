fdd6f8a465217bdb76737a8609cc1b2fadb537bc
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2025-06-12 22:50:50 +0700

    Add nested list examples and access methods in Python

---
+++ nested _list.py
@@ -0,0 +1,51 @@
+#nested list diguanakan untuk mreprentasikan data yang berelasi atau data yang memiliki hubungan satu sama lain
+# Nested list digunakan untuk merepresentasikan struktur data multi-dimensi, seperti matriks (2D array) atau data bertingkat.
+# strukur nested list adalah list didalam list
+
+# contoh nested list
+data_mahasiswa = [
+    ["irfan", "budi", "siti"],
+    [80, 90, 70],
+    ["A", "B", "C"]
+]   
+# cara mengakses data pada nested list
+print(data_mahasiswa[0][0])  # mengakses data pertama pada list pertama     
+print(data_mahasiswa[1][1])  # mengakses data kedua pada list kedua
+print(data_mahasiswa[2][2])  # mengakses data ketiga pada list ketiga
+# cara mengakses data pada nested list dengan perulangan
+
+# contoh pengugunaan nested list pada vektor mahasiswa
+matriks_mahasiswa = [
+    ["Nama", "Nilai", "Kelas"],
+    ["Irfan", 80, "A"],
+    ["Budi", 90, "B"],
+    ["Siti", 70, "C"]
+]       
+for mahasiswa in matriks_mahasiswa:
+      # mencetak data dengan spasi
+    print(mahasiswa)  # mencetak baris baru setelah setiap list
+
+"""Kapan Nested List Berguna?
+Saat mengolah tabel data
+
+Saat membuat matriks
+
+Untuk representasi struktur hierarki (misalnya: menu, pohon keluarga, dsb.)"""
+
+
+
+
+
+
+
+#oot
+for mahasiswa in data_mahasiswa:
+    for data in mahasiswa:
+        print(data, end=" ")  # mencetak data dengan spasi
+    print()  # mencetak baris baru setelah setiap list
+
+# cara mengakses data pada nested list dengan perulangan dan indeks
+for i in range(len(data_mahasiswa)):
+    for j in range(len(data_mahasiswa[i])):
+        print(data_mahasiswa[i][j], end=" ")  # mencetak data dengan spasi
+    print()  # mencetak baris baru setelah setiap list
---
+++ operasi_list.py
@@ -0,0 +1,32 @@
+# didalam bahasa pemmograman python kita bisa mengurutkan data dengan
+# cara mengunakan fungsi sorted dimana fungsi sorted ini akan mengembalikan nilai list yang sudah diurutkan
+# dan tidak mengubah nilai list yang asli
+data_siswa = ["irfan", "budi", "siti", "andi", "cici"]
+data_siswa_urut = sorted(data_siswa) # dengan syntax sorted(nama variabel) dan ouput
+print(data_siswa_urut) # ini akan mengembalikan nilai list yang sudah diurutkan
+
+
+# short di  tipe data angka di list akan mengurukan angka pada list dari terbesar ke terkecil
+data_nilai = [80, 90, 70, 60, 100]
+data_nilai_urut = sorted(data_nilai) # dengan syntax sorted(nama variabel, reverse=True)
+print(data_nilai_urut) # ini akan mengembalikan nilai list yang sudah diurutkan dari terbesar ke terkecil
+
+
+# cara mengabil indeks pada list
+data_siswa = ["irfan", "budi", "siti", "andi", "cici"]  
+indeks_siswa = data_siswa.index("siti") # dengan syntax nama variabel.index(value)
+print(indeks_siswa) # ini akan mengembalikan nilai index dari data yang kita cari   
+
+# reverse pada list berguna untuk membalikan urutan pada list dengan contoh 
+data_siswa.reverse() # dengan syntax nama variabel.reverse()
+print(data_siswa) # ini akan membalikan urutan pada list        
+
+# berlaku pada list dengan tipe data int, float, dan string
+# kita bisa mengunakan kata kunci reversed untuk membalikan urutan pada list
+data_nilai = [80, 90, 70, 60, 100]
+data_nilai_reversed = list(reversed(data_nilai)) # dengan syntax list(reversed(nama variabel))
+print(data_nilai_reversed) # ini akan membalikan urutan pada list
+# kata kunci reversed bisa mengunakan true atau false
+# untuk mengurutkan data pada list
+data_nilai_urut = sorted(data_nilai, reverse=True) # dengan syntax sorted(nama variabel, reverse=True)
+print(data_nilai_urut) # ini akan mengembalikan nilai list yang sudah diurutkan dari terbesar ke terkecil   
