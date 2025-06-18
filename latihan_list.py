80f5a9f8a102c028e03e20ffb6f5bca2cbbb67dc
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2025-06-18 22:44:02 +0700

    Add basic biodata collection program in Python

---
+++ latihan_list.py
@@ -0,0 +1,21 @@
+
+daftar_biodata = []
+
+while True:
+    Nama = input("masukan nama anda: ")
+    umur = input("masukan umur anda : ")
+ 
+    list_biodata = [Nama,umur]
+    daftar_biodata.append(list_biodata)
+    for index, i in enumerate(daftar_biodata):
+        print(f"nomer biodata ke {index+1} dengan {i[0]} dan umur {i[1]}")
+
+    finish = input("apakah anda ingin lajut atau tidak ")
+
+    if finish == "no":
+     break
+ 
+print("progam selesai")
+
+     
+  
