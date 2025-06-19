11bab5d0c0ecef9fd6b844c4519521f5461d1dcb
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2025-06-19 22:52:13 +0700

    Add dictionary data structure example in Python

---
+++ ditionary.py
@@ -0,0 +1,12 @@
+# di pyton ada yang namanya dictionary, yaitu struktur data yang digunakan untuk menyimpan pasangan kunci-nilai.
+# # Dictionary adalah struktur data yang digunakan untuk menyimpan data dalam bentuk pasangan kunci-nilai
+# # Kunci harus unik, dan digunakan untuk mengakses nilai yang terkait.
+# # Dictionary dapat berisi berbagai tipe data, termasuk integer, string, dan list.
+# Contoh penggunaan dictionary
+
+isi_kotak = {
+    "kotak1" : "bolu",
+     "kotak2" : "kue"
+}
+
+print(f"contoh pengunaan dictionary ini adalah kotak yang isinya {isi_kotak['kotak1']}") # yup penganti seprti index gampanya
