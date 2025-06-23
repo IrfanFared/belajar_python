dd0c050be4fb275311995b8af5f0ecadf7be9cb1
Muhammad Irfan Pratama <160725399+IrfanFared@users.noreply.github.com>
2025-06-23 13:53:49 +0700

    Add initial implementation of functions with examples in fungsi.py

---
+++ fungsi.py
@@ -0,0 +1,15 @@
+# jadi apa itu fungsi yaitu adalah sekumpulan kode yang bisa kita panggil berulang kali
+#untuk menhindari penulisan kode yang berulang ulang
+#contoh 
+
+p = True
+q = False
+
+def logika_matematika():
+    if p and q :
+        print("true*12")
+    else :
+        print("hehe")
+
+
+logika_matematika()
